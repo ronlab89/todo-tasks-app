8a1b227604092c2a91d99cc3e8333e97cae5b8cd
Ronald Labrador <ronaldlabrador89@gmail.com>
2022-06-09 17:16:30 -0400

    Layout Welcome view in mobile design

--- src/routes/Welcome.jsx
+++ src/routes/Welcome.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Button from '../components/Button'
 import NavbarWelcome from '../components/NavbarWelcome'
+import listTodo from '../../public/assets/images/listTodo.jpg'
 
 const Welcome = () => {
+
   return (
-    <>
+    <div className='page'>
         <NavbarWelcome />
-        <section className='container row g-0 mt-5 pt-5'>
+        <section className='container row'>
             <article className="col-12">
-                <p>Es momento de organizar tu día a día, se más productivo, agil y eficaz. <br /> ¿Estas list@ para comenzar una revolución en tu forma de gestionar proyectos y tareas?</p>
-                
+                <p className='text-welcome'>Es momento de organizar tu día a día, se más productivo, agil y eficaz.</p>
+                <p className='text-welcome'>¿Estas list@ para comenzar una revolución en tu forma de gestionar proyectos y tareas?</p>
+                <Link to={'/login'}>
+                    <Button type='button' text='Empezar' className='primary-button' />
+                </Link>
             </article>
             <article className="col-12">
-                <img src='' alt="" />
+                <img src={listTodo} alt="Ilustracion de lista de tareas pendientes" className='img-fluid' width='360'/>
             </article>
         </section>
-    </>
+    </div>
   )
 }
 
