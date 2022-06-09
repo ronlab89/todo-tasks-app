af609064d69a535d4920388c5cbfef309fdf3759
Ronald Labrador <ronaldlabrador89@gmail.com>
2022-06-09 16:52:58 -0400

    created Button component and their css file

--- src/routes/Welcome.jsx
+++ src/routes/Welcome.jsx
@@ -5,11 +5,14 @@ const Welcome = () => {
   return (
     <>
         <NavbarWelcome />
-        <section className='container row g-0'>
+        <section className='container row g-0 mt-5 pt-5'>
             <article className="col-12">
                 <p>Es momento de organizar tu día a día, se más productivo, agil y eficaz. <br /> ¿Estas list@ para comenzar una revolución en tu forma de gestionar proyectos y tareas?</p>
+                
             </article>
-            <article className="col-12"></article>
+            <article className="col-12">
+                <img src='' alt="" />
+            </article>
         </section>
     </>
   )
