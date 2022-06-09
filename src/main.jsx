3f71f3adb9e250e92ac5a7fdf3158188c84c3a30
Ronald Labrador <ronaldlabrador89@gmail.com>
2022-06-09 18:27:48 -0400

    Created login view and route to login

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Welcome from './routes/Welcome'
+import Login from './routes/Login'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path='/' element={<App />}>
           <Route index element={<Welcome />} />
+          <Route path='/login' element={<Login />} />
         </Route>
       </Routes>
     </BrowserRouter>
