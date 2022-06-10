5c74033702244f337f1e35d179bbf3b33be017e3
Ronald Labrador <ronaldlabrador89@gmail.com>
2022-06-10 09:59:02 -0400

    Create Input component and their css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import Input from './components/Input'
 
 function App() {
 
---
+++ src/components/Input.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Input = ({label, type, placeholder}) => {
+  return (
+    <div className="form-floating mb-3">
+        <input
+          type={type}
+          className="form-control"
+          id="floatingInput"
+          placeholder={placeholder} 
+        />
+        <label for="floatingInput">{label}</label>
+    </div>
+  )
+}
+
+export default Input
