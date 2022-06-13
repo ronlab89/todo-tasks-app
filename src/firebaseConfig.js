85e8c8e1c99f466274f785103a5d1be63307690b
Ronald Labrador <ronaldlabrador89@gmail.com>
2022-06-13 14:45:25 -0400

    add firebase and config

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDay4XnSSM0ZuJ70lpYQX7MmBrduDM9GuY",
+  authDomain: "todo-tasks-app-dca53.firebaseapp.com",
+  projectId: "todo-tasks-app-dca53",
+  storageBucket: "todo-tasks-app-dca53.appspot.com",
+  messagingSenderId: "300543617478",
+  appId: "1:300543617478:web:5e0d22e7e9da5c2f866ac7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
