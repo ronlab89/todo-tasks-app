15876aed24cc5b4662095e7362d04cf85455d1e5
Ronald Labrador <ronaldlabrador89@gmail.com>
2022-06-18 11:07:52 -0400

    Add moment library and date layout

--- src/components/CreateProject.jsx
+++ src/components/CreateProject.jsx
@@ -5,10 +5,26 @@ import '../css/createProject.css'
 import NavIcon from '../components/NavIcon'
 import Title from '../components/Title'
 import UserIcon from '../components/UserIcon'
+import moment from 'moment'
+import { userContext } from '../context/UserProvider'
 
 const CreateProject = () => {
 
     const {toggleMenu} = useContext(navIconContext);
+    const {user} =useContext(userContext);
+
+    const todayDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+    const todayTime = moment().hours();
+
+    let saidHi;
+
+    if(todayTime >= 4 && todayTime < 12) {
+        saidHi = 'Buenos dias';
+    }else if(todayTime >= 12 && todayTime < 19) {
+        saidHi = 'Buenas tardes';
+    } else if(todayTime >= 19 && todayTime < 4) {
+        saidHi = 'Buenas noches'
+    }
 
   return (
     <section className={`${toggleMenu ? 'page-project-active' : 'page-project'}`}>
@@ -23,6 +39,14 @@ const CreateProject = () => {
                 <UserIcon />
             </div>
         </header>
+            <div className="row mt-5">
+                <div className="col-6">
+                   <span className='hi'>{saidHi} {user.email} </span>  
+                </div>
+                <div className="col-6 text-end">
+                    <span className='date'>{todayDate}</span>
+                </div>
+            </div>
     </section>
   )
 }
