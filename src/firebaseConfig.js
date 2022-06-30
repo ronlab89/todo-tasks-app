8daa6fd6ef659b7cf2d486ecb4dc8f504da5728e
Ronald Labrador <ronaldlabrador89@gmail.com>
2022-06-30 09:53:58 -0400

    Initialize cloud firestore

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDay4XnSSM0ZuJ70lpYQX7MmBrduDM9GuY",
@@ -14,5 +15,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider(app);
+const db = getFirestore(app);
 
-export { auth, provider };
+export { auth, provider, db };
